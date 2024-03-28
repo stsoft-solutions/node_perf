f3b4e62fb30835e4f70f8e750080d229f198407e
Sergey Smirnov <s.smirnov@robopro.team>
2024-03-28 09:36:27 +0200

    .net server

---
+++ net_srv/GrpcBarsService.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Simple;
+
+namespace net_srv;
+
+public class GrpcBarsService(Storage storage) : BarService.BarServiceBase
+{
+    private readonly BarsResponse _response = storage.Response;
+
+    #region Overrides of BarServiceBase
+
+    /// <inheritdoc />
+    public override Task<BarsResponse> GetBars(Empty request, ServerCallContext context)
+    {
+        return Task.FromResult(_response);
+    }
+
+    #endregion
+}
--- net_srv/Program.cs
+++ net_srv/Program.cs
@@ -1,16 +1,21 @@
-using net_srv.Services;
+using net_srv;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddSingleton<Storage>(provider => new Storage());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GrpcBarsService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MapGet("/bars", (Storage storage) => storage.Response);
+app.MapGet("/bars-static", (Storage storage) => storage.StaticResponse);
 
 app.Run();
